a19ef9ee1e917b8d98111c0adcea534853410104
Владислав Бережнов <berezhnovvladislav2012@gmail.com>
2025-02-19 19:19:11 +0300

    Add findByName method

--- src/main/java/ru/berezhnov/cloth/ClothRepository.java
+++ src/main/java/ru/berezhnov/cloth/ClothRepository.java
@@ -3,6 +3,9 @@ package ru.berezhnov.cloth;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClothRepository extends JpaRepository<Cloth, Integer> {
+    Optional<Cloth> findByName(String name);
 }
