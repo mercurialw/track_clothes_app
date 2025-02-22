fa6455c4786626bf89b0356970db201bf0ae23f5
Владислав Бережнов <berezhnovvladislav2012@gmail.com>
2025-02-23 01:42:14 +0300

    Delete useless method rework save method

--- src/main/java/ru/berezhnov/services/UserService.java
+++ src/main/java/ru/berezhnov/services/UserService.java
@@ -5,11 +5,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.berezhnov.models.Cloth;
 import ru.berezhnov.models.ClothType;
+import ru.berezhnov.models.Place;
 import ru.berezhnov.models.User;
 import ru.berezhnov.repositories.ClothRepository;
 import ru.berezhnov.repositories.ClothTypeRepository;
+import ru.berezhnov.repositories.PlaceRepository;
 import ru.berezhnov.repositories.UserRepository;
-import ru.berezhnov.util.TrackClothesAppException;
+import ru.berezhnov.util.AppException;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,40 +23,40 @@ public class UserService {
     private final UserRepository userRepository;
     private final ClothRepository clothRepository;
     private final ClothTypeRepository clothTypeRepository;
+    private final PlaceRepository placeRepository;
 
     @Autowired
-    public UserService(UserRepository userRepository, ClothRepository clothRepository, ClothTypeRepository clothTypeRepository) {
+    public UserService(UserRepository userRepository, ClothRepository clothRepository, ClothTypeRepository clothTypeRepository, PlaceRepository placeRepository) {
         this.userRepository = userRepository;
         this.clothRepository = clothRepository;
         this.clothTypeRepository = clothTypeRepository;
+        this.placeRepository = placeRepository;
     }
 
     public Optional<User> findByEmail(String email) {
         return userRepository.findByEmail(email);
     }
 
-    public List<User> findAll() {
-        return userRepository.findAll();
-    }
-
     @Transactional
     public void save(User user, List<Cloth> list) {
         User persistedUser = userRepository.findByEmail(user.getEmail())
-                .orElseThrow(() -> new TrackClothesAppException("User not found"));
-        list.forEach(cloth -> {
-            cloth.setOwner(persistedUser);
-            ClothType persistedClothType = clothTypeRepository.findByName(cloth.getType().getName())
-                    .orElseThrow(() -> new TrackClothesAppException("Cloth type not found"));
-            cloth.setType(persistedClothType);
-            clothRepository.save(cloth);
+                .orElseThrow(() -> new AppException("User not found"));
+        list.forEach(clothToSave -> {
+            clothToSave.setOwner(persistedUser);
+            ClothType persistedClothType = clothTypeRepository.findByName(clothToSave.getType().getName())
+                    .orElseThrow(() -> new AppException("Cloth type not found"));
+            clothToSave.setType(persistedClothType);
+            Place persistedPlace = placeRepository.findByName(clothToSave.getPlace().getName())
+                    .orElseThrow(() -> new AppException("Place not found"));
+            clothToSave.setPlace(persistedPlace);
+            clothRepository.save(clothToSave);
         });
-
     }
 
     @Transactional
     public void update(User user, String oldName, String newName) {
         clothRepository.findByNameAndOwnerName(oldName, user.getEmail())
-                .orElseThrow(() -> new TrackClothesAppException("Cloth not found"))
+                .orElseThrow(() -> new AppException("Cloth not found"))
                 .setName(newName);
     }
 }
