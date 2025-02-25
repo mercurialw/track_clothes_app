999e7fdeb18b6fc6f1143d387b6bcc7883dcac89
Владислав Бережнов <berezhnovvladislav2012@gmail.com>
2025-02-25 19:18:00 +0300

    Add new query

--- src/main/java/ru/berezhnov/repositories/ClothTypeRepository.java
+++ src/main/java/ru/berezhnov/repositories/ClothTypeRepository.java
@@ -1,13 +1,18 @@
 package ru.berezhnov.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.berezhnov.models.ClothType;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ClothTypeRepository extends JpaRepository<ClothType, Integer> {
     Optional<ClothType> findByName(String name);
     void deleteByName(String name);
+    @Query("from ClothType ct inner join ct.clothes c inner join c.owner u where u.email = :ownerEmail") // select distinct ct
+    List<ClothType> findAllByUserEmail(@Param("ownerEmail") String ownerEmail);
 }
