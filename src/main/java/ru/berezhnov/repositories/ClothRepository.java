d4c53b6a56e428024fc9acf242df513ffb4d7c56
Владислав Бережнов <berezhnovvladislav2012@gmail.com>
2025-02-25 19:16:41 +0300

    Add new query

--- src/main/java/ru/berezhnov/repositories/ClothRepository.java
+++ src/main/java/ru/berezhnov/repositories/ClothRepository.java
@@ -11,6 +11,4 @@ import java.util.Optional;
 @Repository
 public interface ClothRepository extends JpaRepository<Cloth, Integer> {
     Optional<Cloth> findByName(String name);
-    @Query("from Cloth c where c.name = :clothName and c.owner.email = :ownerEmail")
-    Optional<Cloth> findByNameAndOwnerName (@Param("clothName") String clothName, @Param("ownerEmail") String ownerEmail);
 }
