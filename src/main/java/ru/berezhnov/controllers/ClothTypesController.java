0111fa0916b88bda1606b3ee5218bd25e7f46a4a
Владислав Бережнов <berezhnovvladislav2012@gmail.com>
2025-02-25 19:19:36 +0300

    Start to rework controllers mappings

--- src/main/java/ru/berezhnov/controllers/ClothTypesController.java
+++ src/main/java/ru/berezhnov/controllers/ClothTypesController.java
@@ -12,39 +12,44 @@ import ru.berezhnov.models.ClothType;
 import ru.berezhnov.services.ClothTypeService;
 import ru.berezhnov.util.ClothTypeValidator;
 import ru.berezhnov.util.AppException;
+import ru.berezhnov.util.EmailExtractor;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/cloth_type")
-public class ClothTypeController {
+@RequestMapping("/api/cloth_types")
+public class ClothTypesController {
 
     private final ClothTypeService clothTypeService;
     private final ClothTypeValidator clothTypeValidator;
     private final ModelMapper modelMapper;
+    private final EmailExtractor emailExtractor;
 
     @Autowired
-    public ClothTypeController(final ClothTypeService clothTypeService, ClothTypeValidator clothTypeValidator, ModelMapper modelMapper) {
+    public ClothTypesController(final ClothTypeService clothTypeService, ClothTypeValidator clothTypeValidator, ModelMapper modelMapper, EmailExtractor emailExtractor) {
         this.clothTypeService = clothTypeService;
         this.clothTypeValidator = clothTypeValidator;
         this.modelMapper = modelMapper;
+        this.emailExtractor = emailExtractor;
     }
 
     @GetMapping
-    public ResponseEntity<List<ClothTypeDTO>> getClothTypes() {
-        return ResponseEntity.ok(clothTypeService.findAll().stream()
-                .map(this::convertClothTypeToClothTypeDTO).toList());
+    public ResponseEntity<List<ClothTypeDTO>> getClothTypes(@RequestHeader("Authorization") String authHeader) {
+        return ResponseEntity.ok(clothTypeService.findAllByUserEmail(emailExtractor.getUserFromHeader(authHeader))
+                .stream().map(this::convertClothTypeToClothTypeDTO).toList());
     }
 
     @PostMapping
-    public ResponseEntity<?> saveClothType(@RequestBody ClothTypeDTO clothTypeDTO) {
+    public ResponseEntity<?> saveClothType(@RequestBody ClothTypeDTO clothTypeDTO,
+                                           @RequestHeader("Authorization") String authHeader) {
         ClothType clothType = convertClothTypeDTOToClothType(clothTypeDTO);
         Errors errors = new BeanPropertyBindingResult(clothType, "clothType");
         clothTypeValidator.validate(clothType, errors);
         if (errors.hasErrors()) {
             throw new AppException("Cloth type already exists");
         }
-        clothTypeService.save(clothType);
+
+        clothTypeService.save(emailExtractor.getUserFromHeader(authHeader), clothType);
         return ResponseEntity.ok(HttpStatus.CREATED);
     }
 
