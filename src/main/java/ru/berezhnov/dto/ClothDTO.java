cc1b02ecf759710043efbf7b45bfc24044be4238
Владислав Бережнов <berezhnovvladislav2012@gmail.com>
2025-03-18 21:06:05 +0300

    add lombok

--- src/main/java/ru/berezhnov/dto/ClothDTO.java
+++ src/main/java/ru/berezhnov/dto/ClothDTO.java
@@ -1,5 +1,8 @@
 package ru.berezhnov.dto;
 
+import lombok.Data;
+
+@Data
 public class ClothDTO {
     private Integer id;
     private String name;
@@ -7,52 +10,4 @@ public class ClothDTO {
     private String size;
     private ClothTypeDTO type;
     private PlaceDTO place;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhotoUrl() {
-        return photoUrl;
-    }
-
-    public void setPhotoUrl(String photoUrl) {
-        this.photoUrl = photoUrl;
-    }
-
-    public ClothTypeDTO getType() {
-        return type;
-    }
-
-    public void setType(ClothTypeDTO type) {
-        this.type = type;
-    }
-
-    public PlaceDTO getPlace() {
-        return place;
-    }
-
-    public void setPlace(PlaceDTO place) {
-        this.place = place;
-    }
-
-    public String getSize() {
-        return size;
-    }
-
-    public void setSize(String size) {
-        this.size = size;
-    }
 }
